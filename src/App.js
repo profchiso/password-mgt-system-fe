bdf7bf2e5181be06b524cd742b0f927c70fef5ed
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-04-27 05:54:52 +0100

    Feat: implement email validatation

--- src/App.js
+++ src/App.js
@@ -39,72 +39,6 @@ function App() {
 
   const options = useMemo(
     () => ({
-      //   background: {
-      //     color: {
-      //       value: ["#ffffff", "#ff0000", "#00ff00", "#0000ff"],
-      //     },
-      //   },
-      //   fpsLimit: 120,
-      //   interactivity: {
-      //     events: {
-      //       onClick: {
-      //         enable: true,
-      //         mode: "push",
-      //       },
-      //       onHover: {
-      //         enable: true,
-      //         mode: "repulse",
-      //       },
-      //     },
-      //     modes: {
-      //       push: {
-      //         quantity: 4,
-      //       },
-      //       repulse: {
-      //         distance: 200,
-      //         duration: 0.4,
-      //       },
-      //     },
-      //   },
-      //   particles: {
-      //     color: {
-      //       value: "#46edbe",
-      //     },
-      //     links: {
-      //       color: "#ffffff",
-      //       distance: 150,
-      //       enable: true,
-      //       opacity: 0.5,
-      //       width: 1,
-      //     },
-      //     move: {
-      //       direction: "none",
-      //       enable: true,
-      //       outModes: {
-      //         default: "bounce",
-      //       },
-      //       random: false,
-      //       speed: 4,
-      //       straight: false,
-      //     },
-      //     number: {
-      //       density: {
-      //         enable: true,
-      //       },
-      //       value: 80,
-      //     },
-      //     opacity: {
-      //       value: 0.5,
-      //     },
-      //     shape: {
-      //       type: "square",
-      //     },
-      //     size: {
-      //       value: { min: 1, max: 5 },
-      //     },
-      //   },
-      //   detectRetina: true,
-
       particles: {
         color: {
           value: ["#ffffff", "#ff0000", "#00ff00", "#0000ff"],
@@ -175,6 +109,15 @@ function App() {
         message: "Submit",
         type: "error",
         description: "All fields are required!!",
+      });
+      return;
+    }
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+      notification.open({
+        message: "Submit",
+        type: "error",
+        description: "Invalid email address!",
       });
       return;
     }
